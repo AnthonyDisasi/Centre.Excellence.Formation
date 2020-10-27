c809fff19ade4c7a5a0b71259b267e435c8b5abe
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-27 14:39:00 +0200

    Modification de la migration

---
+++ Centre.Excellence.Formation/Migrations/20201027123838__init_Inventaire_3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Centre.Excellence.Formation.Migrations
+{
+    public partial class _init_Inventaire_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quatite",
+                table: "Diagnostiques",
+                newName: "Quantite");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantite",
+                table: "Diagnostiques",
+                newName: "Quatite");
+        }
+    }
+}
