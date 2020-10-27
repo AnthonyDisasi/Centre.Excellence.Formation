ff9c3331b4fd2435f81395f2256dae32624542f2
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-27 14:42:33 +0200

    Ajout de la méthode Update qui sera appelée lors de la demande de modification et qui va nous retourner la vue de modification

--- Centre.Excellence.Formation/Areas/Inventaire/Controllers/MaterielController.cs
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/MaterielController.cs
@@ -76,9 +76,16 @@ namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
         public IActionResult Detail(string id)
         {
             Materiel model = db.Materiels.Include(d => d.Diagnostiques).AsNoTracking().FirstOrDefault(m => m.ID == id);
+            return View(model);
+        }
+
+        public IActionResult Update(string id)
+        {
+            Materiel model = db.Materiels.Find(id);
             LFourniture();
             LLocal();
             return View(model);
         }
+
     }
 }
