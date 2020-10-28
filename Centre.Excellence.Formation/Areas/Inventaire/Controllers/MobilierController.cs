266eb1a522f02276e132e3a61f9dcfcc954edc56
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 00:07:48 +0200

    Ajout de l'injection de dependance dans le controlleur

--- Centre.Excellence.Formation/Areas/Inventaire/Controllers/MobilierController.cs
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/MobilierController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Areas.Inventaire.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
@@ -9,6 +10,12 @@ namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
     [Area("Inventaire")]
     public class MobilierController : Controller
     {
+        private readonly DCInventaire db;
+
+        public MobilierController(DCInventaire _db)
+        {
+            db = _db;
+        }
         public string Index()
         {
             return "Salut";
