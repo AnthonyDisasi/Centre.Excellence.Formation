0ba741f6616dc79ade3b64a94189e0bfe2063b03
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-28 19:18:44 +0200

    Ajout de l'injection de dependance de la db de Inventaire

--- Centre.Excellence.Formation/Areas/Inventaire/Controllers/DiagnostiqueController.cs
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/DiagnostiqueController.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Areas.Inventaire.Data;
+using Centre.Excellence.Formation.Areas.Inventaire.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
 {
     public class DiagnostiqueController : Controller
     {
+        private readonly DCInventaire db;
+
+        public DiagnostiqueController(DCInventaire _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Create(string idmat, string description, string marque)
         {
             ViewBag.IDMateriel = idmat;
             ViewBag.Desc = description;
             ViewBag.Marq = marque;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Diagnostique model, string idmat)
+        {
+            model.MaterielID = idmat;
+            if(ModelState.IsValid)
+            {
+                
+            }
         }
     }
 }
