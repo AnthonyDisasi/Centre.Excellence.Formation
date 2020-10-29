9393fc0c18bd79155f44d97a6f51503807265913
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 16:37:34 +0200

    Ajout du model Formateur

---
+++ Centre.Excellence.Formation/Areas/FormationPlaning/Formateur.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Areas.FormationPlaning
+{
+    public class Formateur
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+
+        public string Nom { get; set; }
+        public string Postnom { get; set; }
+        public string Prenom { get; set; }
+
+        [Required, EnumDataType(typeof(Genre))]
+        public Genre Genre { get; set; }
+
+        public string NiveauEtude { get; set; }
+    }
+}
