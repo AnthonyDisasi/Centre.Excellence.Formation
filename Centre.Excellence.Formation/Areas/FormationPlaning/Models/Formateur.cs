d4631d22848fd0b1429ce650553f3f3fd525a5a9
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 16:39:08 +0200

    Ajout de l'énumeration Genre et poursuit du model Formateur

--- Centre.Excellence.Formation/Areas/FormationPlaning/Models/Formateur.cs
+++ Centre.Excellence.Formation/Areas/FormationPlaning/Models/Formateur.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Centre.Excellence.Formation.Areas.FormationPlaning
+namespace Centre.Excellence.Formation.Areas.FormationPlaning.Models
 {
     public class Formateur
     {
---
+++ Centre.Excellence.Formation/Areas/FormationPlaning/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Areas.FormationPlaning.Models
+{
+    public enum Genre
+    {
+        Homme, Femme
+    }
+}
