9534967f9f234a44f142bf2f2e1d681c61177257
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 12:48:29 +0200

    Création d'un nouveau controlleur pour gérer les fournitures et les locals

---
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/Local_FournitureController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
+{
+    public class Local_FournitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
