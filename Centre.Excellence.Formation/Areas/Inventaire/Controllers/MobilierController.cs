78af042377a9a876c0c928420703bb7669168fda
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 11:36:01 +0200

    Implémentation de la méthode LLocal qui nous retournera une liste item des localisation afin d'être ajoutée dans le mobilier

--- Centre.Excellence.Formation/Areas/Inventaire/Controllers/MobilierController.cs
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/MobilierController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Centre.Excellence.Formation.Areas.Inventaire.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
 {
@@ -16,6 +17,18 @@ namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
         {
             db = _db;
         }
+
+        private void LLocal()
+        {
+            List<SelectListItem> Localisation = (from l in db.Localisations orderby l.Nom ascending select new SelectListItem() { Text = l.Nom, Value = l.Nom }).ToList();
+            ViewBag.Localisations = Localisation;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         public string Index()
         {
             return "Salut";
