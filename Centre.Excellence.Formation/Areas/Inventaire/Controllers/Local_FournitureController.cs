5e92e3ab9afe93f1d72d3b2c2d781ed3986635fa
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 12:52:22 +0200

    Ajout de l'injection de dépendance dans le controlleur

--- Centre.Excellence.Formation/Areas/Inventaire/Controllers/Local_FournitureController.cs
+++ Centre.Excellence.Formation/Areas/Inventaire/Controllers/Local_FournitureController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Areas.Inventaire.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
@@ -9,6 +10,12 @@ namespace Centre.Excellence.Formation.Areas.Inventaire.Controllers
     [Area("Inventaire")]
     public class Local_FournitureController : Controller
     {
+        private readonly DCInventaire db;
+
+        public Local_FournitureController(DCInventaire _db)
+        {
+            db = _db;
+        }
         public IActionResult Index()
         {
             return View();
