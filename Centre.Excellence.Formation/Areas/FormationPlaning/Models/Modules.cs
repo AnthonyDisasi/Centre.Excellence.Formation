f40c5740613f1b13916b63986818471e801adb2d
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 16:13:32 +0200

    Création d'une nouvelle zone area pour le planing des formations, création du dossiers qui comprendra les models de données et création d'un premier model nommé Module

---
+++ Centre.Excellence.Formation/Areas/FormationPlaning/Models/Modules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Areas.FormationPlaning.Models
+{
+    public class Modules
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+
+    }
+}
