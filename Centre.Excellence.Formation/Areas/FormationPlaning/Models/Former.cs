c822514b01b012de5ed5c5a239eebbf03618e0fd
AnthonyDisasi <anthonydisasi@gmail.com>
2020-10-29 17:04:48 +0200

    Ajout du model Former pour relier le model Formateur et le model formation

---
+++ Centre.Excellence.Formation/Areas/FormationPlaning/Models/Former.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Areas.FormationPlaning.Models
+{
+    public class Former
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+
+        public string FormateurID { get; set; }
+        public string FormationID { get; set; }
+
+        public Formateur Formateur { get; set; }
+        public Formation Formation { get; set; }
+    }
+}
