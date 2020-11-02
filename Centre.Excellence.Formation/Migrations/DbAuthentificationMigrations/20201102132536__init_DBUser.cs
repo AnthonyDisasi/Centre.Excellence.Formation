345b9b88c166a1af94af2d96b10084f8d80bcd5d
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-02 15:26:26 +0200

    Ajout de la chaine de connexion du nouveau dbcontext et ajout des migrations

---
+++ Centre.Excellence.Formation/Migrations/DbAuthentificationMigrations/20201102132536__init_DBUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Centre.Excellence.Formation.Migrations.DbAuthentificationMigrations
+{
+    public partial class _init_DBUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
