2340b1315700374684c87f88fdc347c103a661af
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:04:42 +0200

    Implémentation du model RoleModificationModel

--- Centre.Excellence.Formation/Models/RoleModificationModel.cs
+++ Centre.Excellence.Formation/Models/RoleModificationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace Centre.Excellence.Formation.Models
 {
     public class RoleModificationModel
     {
+        [Required(ErrorMessage = "Le rôle est obligatoire, veuillez le remplire"), Display(Name = "Le nom du rôle")]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
     }
 }
