175d5b48e6a8b609e26daa7a5d662020efe74644
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 18:58:49 +0200

    Implémentation du model

--- Centre.Excellence.Formation/Models/CreateModel.cs
+++ Centre.Excellence.Formation/Models/CreateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace Centre.Excellence.Formation.Models
 {
     public class CreateModel
     {
+        [Required(ErrorMessage = "Le nom est obligatoire"), Display(Name = "Nom de l'ustisateur")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Le mail est obligatoire"), Display(Name = "E-mail de l'ustisateur")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire"), Display(Name = "Mot de passe de l'ustisateur")]
+        public string Password { get; set; }
     }
 }
