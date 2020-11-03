816e6cd1f86a47e1e96ca3adcc602a6539cdb33d
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:31:06 +0200

    Implémentation de la méthode Edit qui modifier l'utilisateur en fonction des éléments recu en paramettre et fin de l'implémentation du controlleur Admin

--- Centre.Excellence.Formation/Controllers/AdminController.cs
+++ Centre.Excellence.Formation/Controllers/AdminController.cs
@@ -100,5 +100,50 @@ namespace Centre.Excellence.Formation.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email, string password)
+        {
+            ApplicationUSer user = await UserMana.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await userValidator.ValidateAsync(UserMana, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(UserMana, user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user, password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if ((validEmail.Succeeded && validPass == null) || (validEmail.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await UserMana.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
     }
 }
