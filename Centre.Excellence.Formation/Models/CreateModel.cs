d25b72826f1d17a0960d61f936f9112f7e6e2d8a
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 18:56:45 +0200

    Création d'un nouvel model pour la création de compte des utilisateur

---
+++ Centre.Excellence.Formation/Models/CreateModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Models
+{
+    public class CreateModel
+    {
+    }
+}
