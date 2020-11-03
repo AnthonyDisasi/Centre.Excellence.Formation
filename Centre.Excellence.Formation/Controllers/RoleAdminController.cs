dad0066888bd010b6b88331922fded76280f4459
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:49:25 +0200

    Implémentation de la méthode Delete qui supprimera le role en considerant l'id pris en paramettre

--- Centre.Excellence.Formation/Controllers/RoleAdminController.cs
+++ Centre.Excellence.Formation/Controllers/RoleAdminController.cs
@@ -42,5 +42,36 @@ namespace Centre.Excellence.Formation.Controllers
             }
             return View(name);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role found");
+            }
+            return View("Index", roleManager.Roles);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
