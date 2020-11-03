c6cbbf6e53d93ef0a19e31264674318c5d1a1bad
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 20:10:18 +0200

    Implémentation de la méthode Login qui sera appélée si l'utilisateur n'est pas connécté

--- Centre.Excellence.Formation/Controllers/AccountController.cs
+++ Centre.Excellence.Formation/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Centre.Excellence.Formation.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +19,13 @@ namespace Centre.Excellence.Formation.Controllers
         {
             userManager = userMgr;
             signInManager = signinMgr;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
         }
     }
 }
