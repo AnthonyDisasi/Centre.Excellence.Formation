7ff5264da89be56f4a789fcea569739be631388d
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:00:39 +0200

    Implémentation du model LoginModel

--- Centre.Excellence.Formation/Models/LoginModel.cs
+++ Centre.Excellence.Formation/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Centre.Excellence.Formation.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Le mail est obligatoire")]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Le mot de passe est obligatoire")]
+        [UIHint("password")]
+        public string Password { get; set; }
     }
 }
