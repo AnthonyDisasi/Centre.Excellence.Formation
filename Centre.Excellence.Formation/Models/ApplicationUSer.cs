7957ed15b8238c7be9e4a98a8dd4f428e01b3b74
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 18:53:50 +0200

    Implémentation de la classe application user qui heritera de identityuser pour prendre ses proprietes

---
+++ Centre.Excellence.Formation/Models/ApplicationUSer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Centre.Excellence.Formation.Models
+{
+    public class ApplicationUSer : IdentityUser {  }
+}
