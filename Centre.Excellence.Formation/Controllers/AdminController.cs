50c4fe83a7455893f28f8598cc5627cc1e29aaca
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:21:18 +0200

    Ajout des attributs dans le controlleur admin

--- Centre.Excellence.Formation/Controllers/AdminController.cs
+++ Centre.Excellence.Formation/Controllers/AdminController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Controllers
 {
     public class AdminController : Controller
     {
+        private UserManager<ApplicationUSer> UserMana;
+        private IUserValidator<ApplicationUSer> userValidator;
+        private IPasswordValidator<ApplicationUSer> passwordValidator;
+        private IPasswordHasher<ApplicationUSer> passwordHasher;
+
         public IActionResult Index()
         {
             return View();
