18983e8154b5e088de4b0ff12fe9cf14547d24cd
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 19:44:52 +0200

    Ajout des attributs dans le controlleur

--- Centre.Excellence.Formation/Controllers/RoleAdminController.cs
+++ Centre.Excellence.Formation/Controllers/RoleAdminController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Controllers
 {
     public class RoleAdminController : Controller
     {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUSer> userManager;
+
         public IActionResult Index()
         {
             return View();
