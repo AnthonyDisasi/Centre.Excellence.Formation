875822efc8dc970bcd90d822905d6cd325e41dbd
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 20:08:07 +0200

    Ajout des attributs dans le controlleur

--- Centre.Excellence.Formation/Controllers/AccountController.cs
+++ Centre.Excellence.Formation/Controllers/AccountController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Centre.Excellence.Formation.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Centre.Excellence.Formation.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUSer> userManager;
+        private SignInManager<ApplicationUSer> signInManager;
+
         public IActionResult Index()
         {
             return View();
